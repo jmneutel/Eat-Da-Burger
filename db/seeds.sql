9e1f3198c1c1359b8aab35bc476b9910093c8430
Jared Neutel <Jared@Macintosh-2.local>
2017-05-02 14:58:09 -0700

    completed

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Elk Burger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name, eaten) VALUES ('Plain Burger', true);
+INSERT INTO burgers (name) VALUES ('BLT Burger');
